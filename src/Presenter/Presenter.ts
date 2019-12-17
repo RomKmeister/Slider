cafb29399bfa3e788a805ba98cdf7b1d10f17204
RomK <romank1988@mail.ru>
2019-12-17 23:45:11 +0300

    Добавлены методы установки параметров панели и слайдера. Добавлены методы вставки вьюх

--- src/Presenter/Presenter.ts
+++ src/Presenter/Presenter.ts
@@ -1,28 +1,40 @@
 import Model from '../Model/Model';
-import ViewSlider from '../View/ViewSlider';
+import ViewSlider from '../ViewSlider/ViewSlider';
+import ViewPanel from '../ViewPanel/ViewPanel';
 
 class Presenter {
   model: Model;
 
   viewSlider: ViewSlider;
 
+  viewPanel: ViewPanel;
+
   constructor(options: object) {
     this.model = new Model(options);
     this.viewSlider = new ViewSlider(this.model);
+    this.viewPanel = new ViewPanel(this.model);
     this.model.setMediator(this);
     this.viewSlider.setMediator(this);
+    this.viewPanel.setMediator(this);
+    this.viewSlider.setSliderParameters();
+    this.viewPanel.setPanelParameters();
   }
 
   notify(property: string, value: number | boolean): void {
     this.model.updateModel(property, value);
     this.model.checkScaleBorders();
     this.viewSlider.setSliderParameters();
+    this.viewPanel.setPanelParameters();
+
+  }
+
+  show() {
+    return this.viewPanel.panel;
+  }
+
+  show1() {
+    return this.viewSlider.slider;
   }
 }
-
-
-const slider = new Presenter({});
-slider.viewSlider.setSliderParameters();
-
 
 export default Presenter;
