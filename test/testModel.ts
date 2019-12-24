9e838d94be8afce2eb827979a6e0e3fb92dc3f19
RomK <romank1988@mail.ru>
2019-12-24 20:54:04 +0300

    Добавлены тесты модели

--- test/testModel.ts
+++ test/testModel.ts
@@ -3,30 +3,79 @@ import Model from '../src/Model/Model';
 
 describe("Slider", function() {
 
+
   it("FirstValue should be less than maxValueScale", () => {
-    const valueMoreMax: Model = new Model({
-      maxValueScale: 150,
-      firstValue: 200,
+    let model: Model = new Model({
+      minValueScale: 0,
+      maxValueScale: 100,
+      firstValue: 55,
+      showSecondValue: false,
+      secondValue: 70,
+      step: 1,
+      verticalScale: true,
+      showBubble: true,
     });
-    valueMoreMax.checkScaleBorders();
-    expect(valueMoreMax.firstValue).to.deep.equal(valueMoreMax.maxValueScale)
+    model.updateModel("firstValue", 200);
+    expect(model.firstValue).to.deep.equal(100)
   });
 
   it("FirstValue should be more than minValueScale", () => {
-    const valuedLessMin: Model = new Model({
-      minValueScale: 10,
-      firstValue: 0,
+    let model: Model = new Model({
+      minValueScale: 0,
+      maxValueScale: 100,
+      firstValue: 55,
+      showSecondValue: false,
+      secondValue: 70,
+      step: 1,
+      verticalScale: true,
+      showBubble: true,
     });
-    valuedLessMin.checkScaleBorders();
-    expect(valuedLessMin.firstValue).to.deep.equal(valuedLessMin.minValueScale)
+    model.updateModel("firstValue", -10);
+    expect(model.firstValue).to.deep.equal(0)
+  });
+
+  it("FirstValue should be less than secondValue if it visible", () => {
+    let model: Model = new Model({
+      minValueScale: 0,
+      maxValueScale: 100,
+      firstValue: 55,
+      showSecondValue: true,
+      secondValue: 70,
+      step: 1,
+      verticalScale: true,
+      showBubble: true,
+    });
+    model.updateModel("firstValue", 200);
+    expect(model.firstValue).to.deep.equal(69)
   });
   
   it("SecondValue should be less than maxValueScale", () => {
-    const valueMoreMax: Model = new Model({
-      maxValueScale: 150,
-      secondValue: 200,
+    let model: Model = new Model({
+      minValueScale: 0,
+      maxValueScale: 100,
+      firstValue: 55,
+      showSecondValue: false,
+      secondValue: 70,
+      step: 1,
+      verticalScale: true,
+      showBubble: true,
     });
-    valueMoreMax.checkScaleBorders();
-    expect(valueMoreMax.secondValue).to.deep.equal(valueMoreMax.maxValueScale)
+    model.updateModel("secondValue", 200);
+    expect(model.secondValue).to.deep.equal(100)
+  });
+
+  it("SecondValue should be more than firstValue", () => {
+    let model: Model = new Model({
+      minValueScale: 0,
+      maxValueScale: 100,
+      firstValue: 55,
+      showSecondValue: true,
+      secondValue: 70,
+      step: 1,
+      verticalScale: true,
+      showBubble: true,
+    });
+    model.updateModel("secondValue", 50);
+    expect(model.secondValue).to.deep.equal(56)
   });
 });
