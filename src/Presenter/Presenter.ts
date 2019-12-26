5b83bc3a907393117790ab430e329f10f8d846dd
RomK <romank1988@mail.ru>
2019-12-27 02:59:41 +0300

    Изменен метод уведомлений

--- src/Presenter/Presenter.ts
+++ src/Presenter/Presenter.ts
@@ -9,30 +9,26 @@ class Presenter {
 
   viewPanel: ViewPanel;
 
-  constructor(options: object) {
+  constructor(options: any) {
     this.model = new Model(options);
     this.viewSlider = new ViewSlider(this.model);
     this.viewPanel = new ViewPanel(this.model);
     this.model.setMediator(this);
     this.viewSlider.setMediator(this);
     this.viewPanel.setMediator(this);
+  }
+
+  notify(property: any): void {
+    this.model.updateModel(property);
     this.viewSlider.setSliderParameters();
     this.viewPanel.setPanelParameters();
   }
 
-  notify(property: string, value: number | boolean): void {
-    this.model.updateModel(property, value);
-    this.model.checkScaleBorders();
-    this.viewSlider.setSliderParameters();
-    this.viewPanel.setPanelParameters();
-
-  }
-
-  show() {
+  showPanel(): HTMLElement {
     return this.viewPanel.panel;
   }
 
-  show1() {
+  showSlider(): HTMLElement {
     return this.viewSlider.slider;
   }
 }
