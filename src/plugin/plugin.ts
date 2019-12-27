4288abc9d4215ae6dea41fb68e11a335536c0c6d
RomK <romank1988@mail.ru>
2019-12-27 03:00:40 +0300

    Исправлены ошибки TS

--- src/plugin/plugin.ts
+++ src/plugin/plugin.ts
@@ -1,21 +1,27 @@
 import Presenter from '../Presenter/Presenter';
 
-(function($) {
-$.fn.sliderPlugin = function (options) {
+declare global {
+  interface JQuery {
+    sliderPlugin(options: any): void;
+  }
+}
 
-  const defaultOptions = {
-    minValueScale: 0,
-    maxValueScale: 100,
-    firstValue: 55,
-    showSecondValue: true,
-    secondValue: 70,
-    step: 1,
-    verticalScale: false,
-    showBubble: true,
+(function ($): void {
+  $.fn.sliderPlugin = function (options): void {
+    const defaultOptions = {
+      minValueScale: 0,
+      maxValueScale: 100,
+      firstValue: 55,
+      showSecondValue: true,
+      secondValue: 70,
+      step: 1,
+      verticalScale: false,
+      showBubble: true,
+    };
+
+    const finalOptions = $.extend({}, defaultOptions, options);
+    const presenter = new Presenter(finalOptions);
+
+    $(this).append(presenter.showSlider(), presenter.showPanel());
   };
-
-  const finalOptions = $.extend({}, defaultOptions, options);
-  const presenter = new Presenter(finalOptions);
-
-  $(this).append(presenter.showPanel(), presenter.showSlider());
-}})(jQuery);
+}(jQuery));
