7aaed37f9f6d7e2f27f7b5dbfd8f0bb913102590
RomKmeister <romank1988@mail.ru>
2020-05-18 01:49:37 +0300

    Доработка презентера

--- src/plugin/Presenter/Presenter.ts
+++ src/plugin/Presenter/Presenter.ts
@@ -1,70 +1,35 @@
 import Model from '../Model/Model';
 import ViewSlider from '../View/ViewSlider';
 import ViewPanel from '../View/ViewPanel';
-import ViewScale from '../View/ViewScale';
-import ViewBubbles from '../View/ViewBubbles';
-import ViewHandles from '../View/ViewHandles';
-import { Slider } from '../interfaces';
+import { ModelOptions } from '../interfaces';
 
 class Presenter {
-  options: Slider
-
   model: Model;
 
   viewSlider: ViewSlider;
 
-  viewScale: ViewScale;
-
-  viewHandles: ViewHandles;
-
-  viewBubbles: ViewBubbles;
-
   viewPanel: ViewPanel;
 
-  constructor(element: HTMLElement, options: Slider) {
-    this.options = options;
-    this.model = new Model();
-    this.viewSlider = new ViewSlider(element);
-    this.viewScale = new ViewScale(element);
-    this.viewHandles = new ViewHandles(element);
-    this.viewBubbles = new ViewBubbles(element);
-    this.viewPanel = new ViewPanel(element);
+  constructor(model: Model, viewSlider: ViewSlider, viewPanel: ViewPanel) {
+    this.model = model;
+    this.viewSlider = viewSlider;
+    this.viewPanel = viewPanel;
     this.init();
   }
 
+  update(data: ModelOptions, event: string): void {
+    if (event === 'modelUpdated') {
+      this.viewSlider.setViewParameters();
+      this.viewPanel.setPanelParameters();
+    }
+    if (event === 'viewSliderUpdated') this.model.update(data);
+    if (event === 'viewPanelUpdated') this.model.update(data);
+  }
+
   private init(): void {
-    this.setEventEmitter();
-    this.viewScale.findElements();
-    this.viewHandles.findElements();
-    this.viewBubbles.findElements();
-    this.viewPanel.findElements();
-    this.viewHandles.bindEventListners();
-    this.viewScale.bindEventListners();
-    this.viewPanel.bindEventListners();
-    this.model.setModelParameters(this.options);
-  }
-
-  update(data, event: string): void {
-    if (event === 'modelUpdated') this.setViewParameters(data);
-    if (event === 'viewUpdated') this.model.setModelParameters(data);
-  }
-
-  private setEventEmitter(): void {
     this.model.eventEmitter.attach(this);
-    this.viewScale.eventEmitter.attach(this);
-    this.viewHandles.eventEmitter.attach(this);
+    this.viewSlider.eventEmitter.attach(this);
     this.viewPanel.eventEmitter.attach(this);
-  }
-
-  private setViewParameters(model: Model): void {
-    this.viewScale.model = model;
-    this.viewHandles.model = model;
-    this.viewBubbles.model = model;
-    this.viewPanel.model = model;
-    this.viewScale.setDirection();
-    this.viewHandles.setHandlersParameters();
-    this.viewBubbles.setBubbleParameters();
-    this.viewPanel.setPanelParameters();
   }
 }
 
