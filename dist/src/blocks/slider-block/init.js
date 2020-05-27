88d674148dd8114e6276ef87f9ae2807bb1cacb5
RomKmeister <romank1988@mail.ru>
2020-05-27 17:36:56 +0300

    Updates

--- dist/src/blocks/slider-block/init.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var slider_block_1 = require("./slider-block");
-$(function () {
-    var $slider = $('.js-slider-block');
-    $slider.each(function (index, item) {
-        new slider_block_1.default($(item));
-    });
-});
--- src/app.js
+++
@@ -1,7 +0,0 @@
-function importAll(r) {
-    r.keys().forEach(r);
-}
-importAll(require.context('../src/', true, /\.scss$/));
-importAll(require.context('../src/favicon', true, /\.*$/));
-importAll(require.context('../src/', true, /\.ts$/));
-importAll(require.context('../src/', true, /\.js$/));
