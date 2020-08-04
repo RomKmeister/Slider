c45e4d5267723daa172606f12b56389ab28d41b9
RomKmeister <romank1988@mail.ru>
2020-08-04 12:19:50 +0300

    Исправлены зависимости в тестах #19

--- test/testEventEmitter.ts
+++ test/testEventEmitter.ts
@@ -1,39 +1,27 @@
 import { expect } from 'chai';
 import * as sinon from 'sinon';
-import Model from '../src/plugin/Model/Model';
-import { BaseOptions } from '../src/plugin/interfaces';
+import { Observer } from '../src/plugin/EventEmitter/EventEmitterInterfaces';
 import EventEmitter from '../src/plugin/EventEmitter/EventEmitter';
 
 describe('EventEmitter', () => {
   let eventEmitter: EventEmitter;
-  let model: Model;
-  let options: BaseOptions;
+  let observer: Observer;
   beforeEach(() => {
-    options = {
-      minValue: 0,
-      maxValue: 100,
-      firstValue: 55,
-      isSecondValueVisible: false,
-      secondValue: 70,
-      step: 1,
-      isVertical: true,
-      isBubbleVisible: true,
-      isScaleStepsVisible: true,
+    observer = {
+      update(): void {}
     };
-
     eventEmitter = new EventEmitter();
-    model = new Model(options);
   });
 
   it('Should attach observer', () => {
     const spy = sinon.spy(eventEmitter, 'attach');
-    eventEmitter.attach(model);
+    eventEmitter.attach(observer);
     expect(spy.called).to.equal(true);
   });
 
   it('Should detach observer', () => {
     const spy = sinon.spy(eventEmitter, 'detach');
-    eventEmitter.detach(model);
+    eventEmitter.detach(observer);
     expect(spy.called).to.equal(true);
   });
 });
--- test/testPresenter.ts
+++ test/testPresenter.ts
@@ -7,16 +7,15 @@ import Model from '../src/plugin/Model/Model';
 import { BaseOptions } from '../src/plugin/interfaces';
 
 describe('Presenter', () => {
-  let presenter: any;
+  let presenter: Presenter;
   let model: Model;
   let options: BaseOptions;
   let view: View;
 
   beforeEach(() => {
-    const sandbox = sinon.createSandbox();
     const element = document.createElement('div');
-    element.classList.add('js-slider-block');
-    element.insertAdjacentHTML('afterbegin', '<div class="js-slider"><div class="js-slider__scale"></div><div class="js-slider__steps"><div class="slider__step"></div></div><div class="js-slider__handle"><div class="js-slider__bubble"></div></div><div class="js-slider__handle"><div class="js-slider__bubble"></div></div>');
+    element.classList.add('js-slider');
+    element.insertAdjacentHTML('afterbegin', '<div class="js-slider__scale"></div><div class="js-slider__steps"><div class="slider__step"></div></div><div class="js-slider__runner"><div class="js-slider__bubble"></div></div><div class="js-slider__runner"><div class="js-slider__bubble"></div>');
 
     options = {
       minValue: 0,
@@ -33,19 +32,21 @@ describe('Presenter', () => {
     model = new Model(options);
     view = new View(element, model);
     presenter = new Presenter(model, view);
-    sandbox.spy(model, 'update');
-    sandbox.spy(view, 'setParameters');
   });
 
   it('Should update model', () => {
-    presenter.update(presenter.model, 'viewUpdated');
-    presenter.update(presenter.model, 'newOptions');
-    expect(presenter.model.update.calledTwice).to.equal(true);
+    const newRatio = { firstValueRatio: 1 };
+    const spy = sinon.spy(model, 'update');
+    presenter.update(newRatio, 'viewUpdated');
+    presenter.update(newRatio, 'newOptions');
+    expect(spy.calledTwice).to.equal(true);
   });
 
   it('Should update views', () => {
-    presenter.update(presenter.model, 'modelUpdated');
-    expect(presenter.view.setParameters.called).to.equal(true);
+    const newRatio = { firstValueRatio: 1 };
+    const spy = sinon.spy(view, 'setParameters');
+    presenter.update(newRatio, 'modelUpdated');
+    expect(spy.called).to.equal(true);
   });
 
   it('Should get model options', () => {
@@ -54,8 +55,9 @@ describe('Presenter', () => {
   });
 
   it('Should set model options', () => {
+    const spy = sinon.spy(model, 'update');
     presenter.setOptions({ minValue: 44 });
-    expect(presenter.model.update.called).to.equal(true);
-    expect(presenter.model.options.minValue).to.equal(44);
+    expect(spy.called).to.equal(true);
+    expect(model.options.minValue).to.equal(44);
   });
 });
