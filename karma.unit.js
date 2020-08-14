3fc44d55313c4de754ac356dfb8a940cbb282733
RomKmeister <romank1988@mail.ru>
2020-08-14 13:32:28 +0300

    Исправлено покрытие кода, добавлен вывод в консоль #19

--- karma.unit.js
+++ karma.unit.js
@@ -3,21 +3,31 @@ const webpackConfig = require('./webpack.config');
 module.exports = function (config) {
   config.set({
     basePath: '',
-    frameworks: ['mocha', 'chai', 'sinon'],
+    frameworks: ['mocha', 'chai', 'sinon', 'karma-typescript'],
     files: [
+      'node_modules/jquery/dist/jquery.js',
+      'src/plugin/**/*.ts',
       'test/**/*.ts',
     ],
     exclude: [
     ],
     preprocessors: {
-      'test/**/*.ts': ['webpack', 'coverage'],
+      'src/plugin/**/*.ts': ['karma-typescript', 'coverage'],
+      'test/**/*.ts': ['karma-typescript'],
     },
     webpack: {
       mode: 'development',
       module: webpackConfig.module,
       resolve: webpackConfig.resolve,
     },
-    reporters: ['progress', 'coverage'],
+    reporters: ['progress', 'karma-typescript', 'coverage'],
+    coverageReporter: {
+      reporters: [
+        { type: 'text' },
+        { type: 'text-summary' },
+        { type: 'html' },
+      ],
+    },
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
